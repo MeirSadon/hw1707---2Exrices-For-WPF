5efae4201564e202711dfec4b4403de4aba093f9
מאיר סעדון <45739995+MeirSadon@users.noreply.github.com>
2019-07-22 11:39:45 +0300

    Add files via upload

---
+++ hw1707 - ComboWithCountries-Exrice2/hw1707 - ComboWithCountries-Exrice2/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1707___ComboWithCountries_Exrice2
+{
+    class Country
+    {
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public string ResourceFlagUrl { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Capital}";
+        }
+    }
+}
---
+++ hw1707 - ComboWithCountries-Exrice2/hw1707 - ComboWithCountries-Exrice2/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace hw1707___ComboWithCountries_Exrice2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Country Israel = new Country() { Name = "Israel", Capital = "Jerusalem", ResourceFlagUrl = "/Resource/israel.png" };
+            Country usa = new Country() { Name = "USA", Capital = "Washington", ResourceFlagUrl = "/Resource/usa.png" };
+            InitializeComponent();
+            myCmbx.ItemsSource = new List<Country> { Israel, usa };
+            
+        }
+
+        private void myCmbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
